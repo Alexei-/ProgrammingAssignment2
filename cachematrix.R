fed9fe3b61fc2270688aff4deeaee36ac4df5e7c
Alexei- <leksx_can@yahoo.com>
2014-07-26 20:05:36 +0300

    Assignment 2
    Descriptions for functions are added

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,8 @@
 
 ## Write a short comment describing this function
 
+## This function implements the chace mechanism like in example
+
 makeCacheMatrix <- function(x = matrix()) {
 	  i <- NULL
         set <- function(y) {
@@ -21,6 +23,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## Current function inspects if the chace contains the invers for matrix "x"
+## if value exists inchache it is returned else the inverse matrix is calculated
+## via "solve()" function, its result is stored in chace and then returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
